893fd596b8d284a3ad7fc08a86367fb4c6b92a8a
Sabricetin <sabriyazilim@gmail.com>
2024-09-28 23:58:54 +0300

    Initial Commit

---
+++ Circulus-Swfit-UI/Circulus_Swfit_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Circulus_Swfit_UIApp.swift
+//  Circulus-Swfit-UI
+//
+//  Created by Sabri Çetin on 28.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct Circulus_Swfit_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
